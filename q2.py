2de4321773982fc6db5574d81d9ecf026bda46f4
Aaron Sahar <aaronsahar@gmail.com>
2022-10-03 22:31:03 -0700

    final

--- q2.py
+++ q2.py
@@ -1,10 +1,7 @@
-
-# ******************************
-# Make your Code
-# ******************************
-
-
-# Requirement
-# No need to use list
-# All input values are taken one by one separatively.
-###
+longest = ''
+string = input("Enter a string, or enter 'stop' or 'STOP' to end the program.")
+while string != 'stop' and string != 'STOP':
+    if len(string) > len(longest):
+        longest = string
+    string = input("Enter a string, or enter 'stop' or 'STOP' to end the program.")
+print(longest)
