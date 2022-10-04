9e6d2a9fa8b2f5ed5a90bfc5c2e53d2f58f8a9a1
Aaron Sahar <aaronsahar@gmail.com>
2022-10-03 22:33:18 -0700

    final

--- q2.py
+++ q2.py
@@ -1,7 +1,13 @@
-longest = ''
+from pickletools import stringnl
+
+
 string = input("Enter a string, or enter 'stop' or 'STOP' to end the program.")
+shortest = string
+longest = string
 while string != 'stop' and string != 'STOP':
     if len(string) > len(longest):
         longest = string
+    if len(string) < len(shortest):
+        shortest = string
     string = input("Enter a string, or enter 'stop' or 'STOP' to end the program.")
-print(longest)
+print(longest, shortest)
